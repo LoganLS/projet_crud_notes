a606fd22168f575c594a422c396026cc11778dbd
LoganLS <logan_56@hotmail.fr>
2020-05-07 16:26:16 +0200

    CRUD notes

--- index.js
+++ index.js
@@ -2,6 +2,24 @@
 
 const Hapi = require('@hapi/hapi');
 
+const mysql = require('mysql');
+const express = require('express');
+var app=express();
+const bodyparser=require('body-parser');
+
+app.use(bodyparser.json);
+
+var mysqlConnection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'projet_notes'
+});
+
+app.listen(3000,()=>console.log('Express server is running at port number 3000'));
+app.get('/add')
+
+/*
 const init = async () => {
 
     const server = Hapi.server({
@@ -27,4 +45,4 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
 });
 
-init();
+init();*/
